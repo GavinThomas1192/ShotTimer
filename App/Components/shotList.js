4fa9dc06ca722d76de626be8769cbdadb50d3b1c
GavinThomas1192 <gthomas1192@gmail.com>
2018-08-17 06:53:48 -0700

    upgrade android and reformated shotlist

--- App/Components/shotList.js
+++ App/Components/shotList.js
@@ -1,79 +1,56 @@
 import React, { Component } from "react";
 import {
-    View,
-    Text,
-    StyleSheet,
-    StatusBar,
-    TouchableHighlight, ScrollView
+  Text,
+  ScrollView
 } from "react-native";
-import { Container, Content, List, ListItem, Button } from 'native-base'
+import {List, ListItem } from 'native-base'
 import styles from './Styles/shotList_styles'
 
-
-
-// const stagingPool = []
-
 export default class ShotList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            shotTimes: [],
-            difference: [],
-            completeTimeObject: [],
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      shotTimes: [],
+      difference: [],
+      completeTimeObject: [],
     }
+  }
 
-    componentDidMount() {
+  componentDidMount() {
+    this.setState({ shotTimes: this.props.tickTimes })
+  }
 
-        this.setState({ shotTimes: this.props.tickTimes })
-    }
-
-    componentWillReceiveProps(nextProps) {
-        this.setState({ shotTimes: nextProps.tickTimes })
-    }
-
-    componentDidUpdate() {
-    }
-
-    render() {
-        const stagingPool = []
+  componentWillReceiveProps(nextProps) {
+    this.setState({ shotTimes: nextProps.tickTimes })
+  }
+  
+  renderShotList() {
+    const stagingPool = []
+    return (
+      this.props.tickTimes.map((times, index) => {
+        let difference = (times - this.props.tickTimes[(index - 1)])
+        Number.isNaN(difference) ? difference = 'None' : difference = difference.toFixed(3) 
+        let concatedObject = { shotTime: times.toFixed(3), shotDifference: difference }
+        stagingPool.push(concatedObject)
+        this.props.updateHome(stagingPool)
         return (
-            <ScrollView
-               
-            >
+          <ListItem style={styles.timeText} key={index}>
+            <Text>{"Shot " + (index + 1) + " @ " + times.toFixed(3) + "."}</Text>
+            <Text>{" Difference " + difference}</Text>
+          </ListItem>
+        )
+      })
+    )
+  }
 
-
-                <Text style={styles.timeText}> Result List!</Text>
-                <List style={{ flex: 0 }}>
-                    {this.props.tickTimes.map((ele, index) => {
-                        // let diff/erence;
-                        let difference = (ele - this.props.tickTimes[(index - 1)])
-
-
-
-                        { Number.isNaN(difference) ? difference = 'None' : difference = difference.toFixed(3) }
-
-                        // this.listView.scrollToEnd()
-
-                        let concatedObject = { shotTime: ele.toFixed(3), shotDifference: difference }
-
-                        stagingPool.push(concatedObject)
-                        this.props.updateHome(stagingPool)
-
-                        // this.setState({ completeTimeObject: [...this.state.completeTimeObject, [concatedObject]] })
-
-                        return <ListItem style={styles.timeText} key={index}>
-                            <Text>{"Shot " + (index + 1) + " @ " + ele.toFixed(3) + "."}</Text>
-                            <Text>{" Difference " + difference}</Text>
-                        </ListItem>
-                    })}
-
-                </List>
-
-
-            </ScrollView >
-            // </View>
-            // </Container>
-        );
-    }
+  render() {
+    return (
+      <ScrollView>
+        <Text style={styles.timeText}> Result List!</Text>
+        <List style={{ flex: 0 }}>
+          {this.renderShotList()}
+        </List>
+      </ScrollView >
+    );
+  }
 }
--- android/app/src/main/java/com/shottimer/MainApplication.java
+++ android/app/src/main/java/com/shottimer/MainApplication.java
@@ -3,6 +3,9 @@ package com.shottimer;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.zmxv.RNSound.RNSoundPackage;
+import com.corbt.keepawake.KCKeepAwakePackage;
+import com.rnim.rn.audio.ReactNativeAudioPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +25,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RNSoundPackage(),
+            new KCKeepAwakePackage(),
+            new ReactNativeAudioPackage()
       );
     }
 
